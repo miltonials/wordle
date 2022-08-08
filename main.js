ea7cec790e9f328fc43551d366bb984ef9981c4e
Andy Porras <andyporras@estudiantec.cr>
2022-08-07 22:21:41 -0600

    Funcion de borrado mejorado
    Se le agrego otra función al botón de borrar

--- main.js
+++ main.js
@@ -26,15 +26,21 @@ function letra(letter, id) {
 
 
 function borrar() {
-
   if (letra_actual.columna > 0 && letra_actual.columna <= 5) {
     letra_actual.columna -= 1;
     cuadro = document.getElementById("cuadro_" + letra_actual.fila + "_" + letra_actual.columna)
+    letraABorrar = cuadro.innerHTML
+    index = letraABorrar.indexOf(">")
+    letraABorrar = letraABorrar.slice(index + 1, letraABorrar.length - 4)
+    // console.log(letraABorrar)
+    boton = document.getElementById("letra" + letraABorrar)
+    boton.style.color = 'black'
+    boton.style.backgroundColor = 'rgb(214, 216, 218)'
+    boton.style.border = 'transparent'
     cuadro.innerHTML = '';
   } else {
     console.log('No hay más letras')
   }
-
 }
 
-console.log(listapalabras)
+// console.log(listapalabras)
