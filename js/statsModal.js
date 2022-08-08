ef1c9ca5515189cd3b10f2dc4c0aba382d3a2e79
Milton Barrera <miltonjosue2001@gmail.com>
2022-08-08 01:40:21 -0600

    Diseño inicial

---
+++ js/statsModal.js
@@ -0,0 +1,9 @@
+const stats = document.getElementById("stats-btn");
+
+stats.addEventListener("click", () => {
+    printModal(`
+    <section>
+      <h1>ESTADÍSTICAS</h1>
+    </section>
+    `); 
+});
