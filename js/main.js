9afb5fdd1865d784b52afa7c4da912680562437e
Andy Porras <andyporras@estudiantec.cr>
2022-08-09 18:04:45 -0600

    validación del enter y correcion del tablero

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ letra_actual = {
   columna: 0
 }
 
+num = Math.random()
+numRando = num 
+console.log(numRando)
+
 
 function letra(letter, id) {
   console.log(listapalabras)
@@ -34,13 +38,58 @@ function borrar() {
     letraABorrar = letraABorrar.slice(index + 1, letraABorrar.length - 4)
     // console.log(letraABorrar)
     boton = document.getElementById("letra" + letraABorrar)
-    boton.style.color = 'black'
-    boton.style.backgroundColor = 'rgb(214, 216, 218)'
+    boton.style.color = '#fff'
+    boton.style.backgroundColor = '#767679'
     boton.style.border = 'transparent'
     cuadro.innerHTML = '';
   } else {
     console.log('No hay más letras')
   }
 }
+
+function enter() {
+  if (letra_actual.fila > 4) {
+    console.log('No hay más filas')
+  }
+  else {
+    console.log(letra_actual.columna)
+    if (letra_actual.columna == 5) {
+      // palabraActual =  "PULPO"
+      palabraActual = document.getElementById("cuadro_" + letra_actual.fila + "_" + 0).innerHTML
+      index = palabraActual.indexOf(">")
+      palabraActual2 = palabraActual.slice(index + 1, palabraActual.length - 4)
+      palabraActual = document.getElementById("cuadro_" + letra_actual.fila + "_" + 1).innerHTML
+      index = palabraActual.indexOf(">")
+      palabraActual2 += palabraActual.slice(index + 1, palabraActual.length - 4)
+      palabraActual = document.getElementById("cuadro_" + letra_actual.fila + "_" + 2).innerHTML
+      index = palabraActual.indexOf(">")
+      palabraActual2 += palabraActual.slice(index + 1, palabraActual.length - 4)
+      palabraActual = document.getElementById("cuadro_" + letra_actual.fila + "_" + 3).innerHTML
+      index = palabraActual.indexOf(">")
+      palabraActual2 += palabraActual.slice(index + 1, palabraActual.length - 4)
+      palabraActual = document.getElementById("cuadro_" + letra_actual.fila + "_" + 4).innerHTML
+      index = palabraActual.indexOf(">")
+      palabraActual2 += palabraActual.slice(index + 1, palabraActual.length - 4)
+      palabraActual2 = palabraActual2.toLowerCase()
+      // + document.getElementById("cuadro_" + letra_actual.fila + "_" + 1).innerHTML + document.getElementById("cuadro_" + letra_actual.fila + "_" + 2).innerHTML + document.getElementById("cuadro_" + letra_actual.fila + "_" + 3).innerHTML + document.getElementById("cuadro_" + letra_actual.fila + "_" + 4).innerHTML
+      console.log(7)
+      console.log(palabraActual2)
+      if (listapalabras.includes(palabraActual2)) {
+        document.getElementById("cuadro_" + letra_actual.fila + "_" + 0).style.backgroundColor = 'green'
+        document.getElementById("cuadro_" + letra_actual.fila + "_" + 1).style.backgroundColor = 'green'
+        document.getElementById("cuadro_" + letra_actual.fila + "_" + 2).style.backgroundColor = 'green'
+        document.getElementById("cuadro_" + letra_actual.fila + "_" + 3).style.backgroundColor = 'green'
+        document.getElementById("cuadro_" + letra_actual.fila + "_" + 4).style.backgroundColor = 'green'
+        console.log('Correcto')
+      }
+      letra_actual.fila += 1;
+      letra_actual.columna = 0;
+    }
+    else {
+      console.log('No hay 5 letras en la fila')
+    }
+  }
+}
+
 
 // console.log(listapalabras)
